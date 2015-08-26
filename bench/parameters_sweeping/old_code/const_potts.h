a39f5fc69a1f7350ec13cc8f2857c80db94e2870
Leonardo Romor <leonardo.romor@gmail.com>
2015-08-26 21:24:00 +0200

    final version of lc, (for now)

--- bench/parameters_sweeping/old_code/const_potts.h
+++ bench/parameters_sweeping/old_code/const_potts.h
@@ -1,7 +1,7 @@
 
 #define      Retr   		3
-#define N 600
-#define Cm 100
+#define N 1500
+#define Cm 27
 #define      p				10
 #define      S				3
 #define      a				0.25f
--- lib/lc_pnet.cpp
+++ lib/lc_pnet.cpp
@@ -23,18 +23,12 @@ LC_PNet::LC_PNet(const int & N, const int & C, const int & S) : N(N),C(C),S(S){
     this->cm = new int[N * C];
     this->ucm = new int[N * N];
     this->J = new __fpv[N * S * C * S]; //!"£"
-    this->states = new __fpv*[N];
-    this->r = new __fpv*[N];
+    this->inactive_states = new __fpv[N];
+    this->active_states = new __fpv[N * S];
+    this->inactive_r = new __fpv[N];
+    this->active_r = new __fpv[N * S];
     this->h = new __fpv[N*S];
     this->theta = new __fpv[N*S];
-
-    //Init r and states
-    for(i = 0; i < N; ++i){
-        this->states[i] = new __fpv[S+1];
-    }
-    for(i = 0; i < N; ++i){
-        this->r[i] = new __fpv[S+1];
-    }
 
     //Init to 0 "1-0 matrix"
     for(i = 0; i < N; ++i){
@@ -47,24 +41,15 @@ LC_PNet::LC_PNet(const int & N, const int & C, const int & S) : N(N),C(C),S(S){
 
 LC_PNet::~LC_PNet(){
 
-    int i;
-
     delete[] this->cm;
     delete[] this->ucm;
     delete[] this->J;
+    delete[] this->inactive_states;
+    delete[] this->active_states;
+    delete[] this->inactive_r;
+    delete[] this->active_r;
     delete[] this->h;
     delete[] this->theta;
-
-
-    for(i = 0; i < N; ++i){
-        delete[] this->states[i];
-    }
-    delete[] this->states;
-
-    for(i = 0; i < N; ++i){
-        delete[] this->r[i];
-    }
-    delete[] this->r;
 
 }
 
@@ -99,11 +84,11 @@ void LC_PNet::init_states(const __fpv & beta, const __fpv & U){
     for(i = 0; i < this->N; ++i){
 
         for(j = 0; j < this->S; ++j){
-            this->states[i][j] = n / d;
+            this->active_states[i * S + j] = n / d;
         }
 
-        this->states[i][S] = 1 - this->S*this->states[i][0];
-        this->r[i][S] = 1 - this->states[i][S];
+        this->inactive_states[i] = 1 - this->S*this->active_states[i * S];
+        this->inactive_r[i] = 1 - this->inactive_states[i];
     }
 }
 void LC_PNet::init_J(const int & p, const __fpv & a, const int * xi){
@@ -128,13 +113,13 @@ void LC_PNet::init_J(const int & p, const __fpv & a, const int * xi){
 
                     this->J[S*C*S*i + C*S*j + S*k + l] /= (__fpv)(a * (1.0 - as)*(__fpv)C);
 
-                    this->h[S*i + j] += this->J[S*C*S*i + C*S*j + S*k + l] * this->states[cm[C*i + k]][l];
+                    this->h[S*i + j] += this->J[S*C*S*i + C*S*j + S*k + l] * this->active_states[S*cm[C*i + k] + l];
 
                 }
             }
 
-            this->r[i][j] = this->h[S*i + j];
-            this->theta[S*i + j] = this->states[i][j];
+            this->active_r[S*i + j] = this->h[S*i + j];
+            this->theta[S*i + j] = this->active_states[S*i + j];
 
         }
     }
@@ -155,61 +140,64 @@ void LC_PNet::init_network(const __fpv & beta, const __fpv & U, const int & p, c
 
 void LC_PNet::update_rule(const int & unit, const __fpv buffer[], const int & pattern, const __fpv & U, const __fpv & w, const __fpv & g, const __fpv & tau, const __fpv & b1, const __fpv & b2, const __fpv & b3, const __fpv & beta, const int & tx, const int & t){
 
-    // //tx == n0 in the old code, "time 'x' "
-    // int i,j;
-    // __fpv self=0, INcost, rmax, Z;
-    // int tsize = this->C * this->S;
-    //
-    // rmax = this->inactive_r[unit];
-    //
-    // for(i = 0; i < this->S; ++i){
-    //     self += this->active_states[unit*S + i];
-    //     this->h[unit*S + i] = 0; //Maybe h can be even a small array of size S, since it's erased at each update
-    // }
-    //
-    // self = (w / this->S) * self;
-    //
-    // INcost = (t > tx) * g * exp(-(t-tx)/tau);
-    //
-    // for(i = 0; i < this->S; ++i){
-    //
-    //     //Inside here maybe different order of + and * so slightly different solutions, have to check.
-    //     for(j = 0; j < tsize; ++j){
-    //         this->h[unit*S + i] += this->J[S*C*S*unit + C*S*i + j] * buffer[j];
-    //     }
-    //
-    //     this->h[unit*S + i] += w * this->active_states[unit*S + i] - self + INcost * (pattern == i);
-    //
-    //     this->theta[unit*S + i] += b2 * (this->active_states[unit*S + i]-this->theta[unit*S + i]);
-	//     this->active_r[unit*S + i] += b1 * (this->h[unit*S + i]-this->theta[unit*S + i]-this->active_r[unit*S + i]);
-    //
-    //     rmax = this->active_r[unit*S + i] * (this->active_r[unit*S + i] > rmax) - ((this->active_r[unit*S + i] > rmax) - 1) * rmax;
-    //
-    //     /*
-    //     if(this->active_r[unit*S + i]>rmax){
-    //         rmax=this->active_r[unit*S + i];
-	//     }
-    //     */
-    //
-    //
-    // }
-    //
-    // this->inactive_r[unit] += b3 * (1.0 - this->inactive_states[unit] - this->inactive_r[unit]);
-    //
-    // Z=0;
-    //
-    // for(i = 0; i < S; ++i){
-    //     Z += exp(beta * (this->active_r[unit*S + i] - rmax));
-    // }
-    //
-    // Z += exp(beta * (this->inactive_r[unit] + U - rmax));
-    //
-    //
-    // for(i = 0; i < S; ++i){
-    // 	this->active_states[unit*S + i] = exp(beta * (this->active_r[unit*S + i] - rmax)) / Z;
-    // }
-    //
-    // this->inactive_states[unit]=exp(beta * (this->inactive_r[unit] - rmax + U)) / Z;
+    //tx == n0 in the old code, "time 'x' "
+    int i,j;
+    __fpv self=0, INcost, rmax, Z;
+    int tsize = this->C * this->S;
+    const __fpv tb3 = b3;
+    const __fpv tb1 = b1;
+    const __fpv tb2 = b2;
+
+    rmax = this->inactive_r[unit];
+
+    for(i = 0; i < this->S; ++i){
+        self += this->active_states[unit*S + i];
+        this->h[unit*S + i] = 0; //Maybe h can be even a small array of size S, since it's erased at each update
+    }
+
+    self = (w / this->S) * self;
+
+    INcost = (t > tx) * g * exp(-(t-tx)/tau);
+
+    for(i = 0; i < this->S; ++i){
+
+        //Inside here maybe different order of + and * so slightly different solutions, have to check.
+        for(j = 0; j < tsize; ++j){
+            this->h[unit*S + i] += this->J[S*C*S*unit + C*S*i + j] * buffer[j];
+        }
+
+        this->h[unit*S + i] += w * this->active_states[unit*S + i] - self + INcost * (pattern == i);
+
+        this->theta[unit*S + i] += tb2 * (this->active_states[unit*S + i]-this->theta[unit*S + i]);
+	    this->active_r[unit*S + i] += tb1 * (this->h[unit*S + i]-this->theta[unit*S + i]-this->active_r[unit*S + i]);
+
+        rmax = this->active_r[unit*S + i] * (this->active_r[unit*S + i] > rmax) - ((this->active_r[unit*S + i] > rmax) - 1) * rmax;
+
+        /*
+        if(this->active_r[unit*S + i]>rmax){
+            rmax=this->active_r[unit*S + i];
+	    }
+        */
+
+
+    }
+
+    this->inactive_r[unit] += tb3 * (1.0 - this->inactive_states[unit] - this->inactive_r[unit]);
+
+    Z=0;
+
+    for(i = 0; i < S; ++i){
+        Z += exp(beta * (this->active_r[unit*S + i] - rmax));
+    }
+
+    Z += exp(beta * (this->inactive_r[unit] + U - rmax));
+
+
+    for(i = 0; i < S; ++i){
+    	this->active_states[unit*S + i] = exp(beta * (this->active_r[unit*S + i] - rmax)) / Z;
+    }
+
+    this->inactive_states[unit]=exp(beta * (this->inactive_r[unit] - rmax + U)) / Z;
 
 
 }
@@ -226,9 +214,6 @@ void LC_PNet::start_dynamics(std::default_random_engine & generator, const int &
     bool stop = false;
     int Mumax = p + 5, Mumaxold = p + 5, steps = 0;
 
-    int l,m,ts;
-    __fpv self=0, INcost, rmax, Z;
-    int tsize = this->C * this->S;
     const __fpv tb3 = b3;
 
     t = 0;
@@ -246,14 +231,13 @@ void LC_PNet::start_dynamics(std::default_random_engine & generator, const int &
 
             unit = sequence.get(j);
 
+            //Fill the buffer containing all the states requested
             for(k = 0; k < this->C; ++k){
                 for(n = 0; n < this->S; ++n){
-                    buffer[k*S + n] = this->states[cm[unit * C + k]][n];
+                    buffer[k*S + n] = this->active_states[S*cm[unit * C + k] + n];
                 }
             }
 
-
-            /*
             //Update the unit
             this->update_rule(unit,
                              buffer,
@@ -264,69 +248,12 @@ void LC_PNet::start_dynamics(std::default_random_engine & generator, const int &
                              tau,
                              b1,
                              b2,
-                             b3,
+                             tb3,
                              beta,
                              tx,
                              t
                              );
-            */
-            ////////////////////////////////////////////////////////////////////
-            //tx == n0 in the old code, "time 'x' "
-            rmax = this->r[unit][S];
-            self = 0;
 
-            for(l = 0; l < this->S; ++l){
-                self += this->states[unit][l];
-                this->h[unit*S + l] = 0; //Maybe h can be even a small array of size S, since it's erased at each update
-            }
-
-            self = (w / this->S) * self;
-
-            INcost = (t > tx) * g * exp(-(t-tx)/tau);
-
-            for(l = 0; l < this->S; ++l){
-
-                //Inside here maybe different order of + and * so slightly different solutions, have to check.
-                for(m = 0; m < tsize; ++m){
-                    this->h[unit*S + l] += this->J[S*C*S*unit + C*S*l + m] * buffer[m];
-                }
-
-
-                this->h[unit*S + l] += w * this->states[unit][l] - self + INcost * (xi[p * unit + pattern] == l);
-
-                this->theta[unit*S + l] += b2 * (this->states[unit][l]-this->theta[unit*S + l]);
-                this->r[unit][l] += b1 * (this->h[unit*S + l]-this->theta[unit*S + l]-this->r[unit][l]);
-
-                rmax = this->r[unit][l] * (this->r[unit][l] > rmax) - ((this->r[unit][l] > rmax) - 1) * rmax;
-
-                /*
-                if(this->active_r[ts + i]>rmax){
-                    rmax=this->active_r[ts + i];
-                }
-                */
-
-
-            }
-
-            this->r[unit][S] += tb3*(1.0 - this->states[unit][S] - this->r[unit][S]);
-
-            Z=0;
-
-            for(l = 0; l < S; ++l){
-                Z += exp(beta * (this->r[unit][l] - rmax));
-            }
-
-            Z += exp(beta * (this->r[unit][S] + U - rmax));
-
-
-            for(l = 0; l < S; ++l){
-                this->states[unit][l] = exp(beta * (this->r[unit][l] - rmax)) / Z;
-            }
-
-            this->states[unit][S]=exp(beta * (this->r[unit][S] - rmax + U)) / Z;
-
-
-            ////////////////////////////////////////////////////////////////////
 
             if((t % tstatus) == 0){
                 latching_length = (double)t / N;
@@ -362,7 +289,7 @@ void LC_PNet::evaluate_m(const int & p, const __fpv & a, const int * xi, __fpv m
         for(j = 0; j < N; ++j){
             ma = 0;
             for(k = 0; k < S; ++k){
-                ma += ( (xi[p * j + i] == k) - a/S) * this->states[j][k];
+                ma += ( (xi[p * j + i] == k) - a/S) * this->active_states[S*j + k];
             }
             maa += ma;
         }
@@ -411,9 +338,9 @@ void LC_PNet::save_states_to_file(const std::string & filename){
 
     for(i = 0; i < this->N; i++){
         for(j= 0; j < this->S; j++){
-            ofile << this->states[i][j]<< " ";
+            ofile << this->active_states[S*i + j]<< " ";
         }
-        ofile << this->states[i][S]<< " ";
+        ofile << this->inactive_states[i]<< " ";
         ofile << std::endl;
     }
     ofile.close();
